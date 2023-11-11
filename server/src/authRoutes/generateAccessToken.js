c0e65f39ee09a298e71992f19f6d36a635cd2357
alejandro-mancebo <lazaro.manceboariosa@georgebrown.ca>
2023-11-10 20:43:09 -0500

    delete authServer.js, index.js, authRoutes/authLoginRoutes.js, authRoutes/generateAccessToken.js, data/users.data.js

--- server/src/authRoutes/generateAccessToken.js
+++
@@ -1,6 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-export function GenerateAccessToken(userId) {
-  const token = jwt.sign(userId, process.env.ACCESS_TOKEN_SECRET);
-  return jwt.sign({ userId: userId }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
-}
